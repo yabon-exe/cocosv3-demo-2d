644e683db5dcc3b38664b268262b86752989d623
yabon_exe <yabon116@gmail.com>
2023-03-03 11:51:24 +0900

    U

---
+++ assets/object/player/Player.ts
@@ -0,0 +1,37 @@
+import { _decorator, Component, RigidBody2D, find, v2, Collider2D, Contact2DType, ColliderComponent } from 'cc';
+const { ccclass, property } = _decorator;
+
+/**
+* プレイヤー
+*/
+@ccclass('Player')
+export default class Player extends Component {
+    touchController: any = null;
+    rigidBody: RigidBody2D = null;
+    onLoad() {
+
+        // touch-controllerを取得する
+        this.touchController = find("TouchController", this.node.parent).getComponent('TouchController');
+
+        // 自身のボディを取得する
+        this.rigidBody = this.node.getComponent(RigidBody2D);
+
+
+    }
+    
+    start() {
+
+
+    }
+    update() {
+
+        // touch-controllerの入力を読み取って動作する
+        if (this.touchController.swiping) {
+            let vel = this.touchController.swipeVec;
+            this.rigidBody.linearVelocity = vel;
+        } else {
+            this.rigidBody.linearVelocity = v2(0, 0);
+        }
+    }
+}
+
