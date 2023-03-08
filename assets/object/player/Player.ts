15b242bb8d0e4e5f351cd89896f2172165d73353
yabon_exe <yabon116@gmail.com>
2023-03-08 20:08:31 +0900

    U

--- assets/object/player/Player.ts
+++ assets/object/player/Player.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, RigidBody2D, find, v2, Collider2D, Contact2DType, tween } from 'cc';
+import { _decorator, Component, RigidBody2D, find, v2, Collider2D, Contact2DType, tween, Tween, Node } from 'cc';
 import { Property } from '../property/Property'
 const { ccclass } = _decorator;
 
@@ -11,6 +11,16 @@ export default class Player extends Component {
     rigidBody: RigidBody2D = null;
 
     /**
+     * ダメージモーション
+     */
+    damageTween: Tween<Node>;
+
+    /**
+     * ダメージによるストップフラグ
+     */
+    damageStopping: boolean = false;
+
+    /**
      * 衝突処理
      * 
      * @param selfCollider 自身の接触オブジェクト
@@ -20,19 +30,10 @@ export default class Player extends Component {
     onBeginContact(selfCollider: Collider2D, otherCollider: Collider2D, contact: any): void {
 
         if (otherCollider.tag == 10) {
-            // 揺れている期間
-            const duration:number  = 0.1;
-            // 揺れ幅
-            const hitAngle:number = 20;  
-
-            tween(this.node)
-                .to(duration / 2.0, { angle: -hitAngle })
-                .to(duration, { angle: hitAngle })
-                .to(duration, { angle: -hitAngle })
-                .to(duration, { angle: hitAngle })
-                .to(duration, { angle: -hitAngle })
-                .to(duration / 2.0, { angle: 0 })
-                .start();   
+            // ダメージを受ける
+            this.damageStopping = true;
+            // 弾丸に当たったら振動する
+            this.damageTween.start();   
         }
     }
 
@@ -46,16 +47,36 @@ export default class Player extends Component {
         const collider = this.getComponent(Collider2D);
         if (collider) {
             // タグを設定する
-            let property = find('Property', this.node.parent).getComponent('Property') as Property;
+            let property = find('Property', this.node.parent).getComponent(Property);
             let tag = property.getTag('NONE');
             collider.tag = tag;
             collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
         }
+
+        // 揺れている期間
+        const duration:number  = 0.03;
+        // 揺れ幅
+        const hitAngle:number = 20;
+        //ダメージモーション設定
+        this.damageTween = tween(this.node)
+            .to(duration / 2.0, { angle: -hitAngle })
+            .to(duration, { angle: hitAngle })
+            .to(duration, { angle: -hitAngle })
+            .to(duration, { angle: hitAngle })
+            .to(duration, { angle: -hitAngle })
+            .to(duration / 2.0, { angle: 0 })
+            .call(() => {
+                // モーション終了時にダメージによるストップフラグを解除する
+                this.damageStopping = false;
+            });   
     }
     update() {
 
-        // touch-controllerの入力を読み取って動作する
-        if (this.touchController.swiping) {
+        if (this.damageStopping) {
+            // ダメージモーション中は停止
+            this.rigidBody.linearVelocity = v2(0, 0);
+        } else if (this.touchController.swiping) {
+            // touch-controllerの入力を読み取って動作する
             let vel = this.touchController.swipeVec;
             this.rigidBody.linearVelocity = vel;
         } else {
